530f0408ff18f87c37940f698d923a451a27c13d
EugeneKur <ew.kurilov@gmail.com>
2022-01-17 08:31:47 +0300

    homework9.1

--- app/src/main/java/com/example/bestmovie2/view/ContactsActivity.kt
+++ app/src/main/java/com/example/bestmovie2/view/ContactsActivity.kt
@@ -20,6 +20,7 @@ class ContactsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_contacts)
+
         checkPermission()
     }
 
